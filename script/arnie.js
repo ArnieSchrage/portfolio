99248b32e6b2932a21775ae99077fd592da03f97
ArnieSchrage <arnie.schrage@gmail.com>
2021-01-25 17:07:30 +0100

    mobile update

--- script/arnie.js
+++ script/arnie.js
@@ -1,3 +1,4 @@
+//Reset video to start after new slide transition
 $("#mywork-carousel").on("slide.bs.carousel", function (e) {
   var vid = $(e.relatedTarget).find("video");
   console.log(vid);
@@ -5,3 +6,15 @@ $("#mywork-carousel").on("slide.bs.carousel", function (e) {
     vid[0].load();
   }
 });
+
+//Toggle script mobile nav
+var el = document.querySelectorAll(".hamburger");
+for (i = 0; i <= el.length; i++) {
+  el[i].addEventListener(
+    "click",
+    function () {
+      this.classList.toggle("active");
+    },
+    false
+  );
+}
