69e403cc5940772e5c0792f6e72c9bad9759586e
ArnieSchrage <arnie.schrage@gmail.com>
2021-01-27 14:10:53 +0100

    up

--- script/resetvideo.js
+++ script/resetvideo.js
@@ -8,3 +8,7 @@ window.onload = function () {
     }
   });
 };
+
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
--- script/toggle.js
+++ script/toggle.js
@@ -1,19 +1,16 @@
-window.onload = function () {
-  //Toggle script mobile nav
-  var el = document.querySelectorAll(".hamburger");
-  var main = document.querySelectorAll(".sect");
+//Toggle script mobile nav
+var el = document.querySelectorAll(".hamburger");
+var main = document.querySelectorAll(".sect");
 
-  for (i = 0; i <= el.length; i++) {
-    el[i].addEventListener(
-      "click",
-      function () {
-        console.log(main);
-        this.classList.toggle("active");
-        for (let i = 0; i < main.length; i += 1) {
+for (i = 0; i <= el.length; i++) {
+  el[i].addEventListener(
+    "click",
+    function () {
+      this.classList.toggle("active");
+      /*for (let i = 0; i < main.length; i += 1) {
           main.item(i).classList.toggle("blurred");
-        }
-      },
-      false
-    );
-  }
-};
+        }*/
+    },
+    false
+  );
+}
